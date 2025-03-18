f44b0cf52a9981f33fe90609d46278844d7203fa
bilalsarwar2907 <bilalsarwar2907@gmail.com>
2025-03-18 13:18:01 +0100

    test

--- GasolinWebsite/Pages/GasolinBiography.cshtml.cs
+++ GasolinWebsite/Pages/GasolinBiography.cshtml.cs
@@ -9,10 +9,10 @@ public class GasolinBiographyModel : PageModel
     {
         Members = new List<BandMember>
         {
-            new BandMember { Name = "Kim Larsen", Role = "Vocals, Guitar", Biography = "Kim Larsen was the lead singer and guitarist of Gasolin, known for his poetic lyrics and charismatic stage presence." },
-            new BandMember { Name = "Franz Beckerlee", Role = "Guitar", Biography = "Franz Beckerlee was the band's lead guitarist, famous for his raw and energetic playing style." },
-            new BandMember { Name = "Wili Jønsson", Role = "Bass", Biography = "Wili Jønsson played bass and contributed to the band's unique sound with his melodic style." },
-            new BandMember { Name = "Søren Berlev", Role = "Drums", Biography = "Søren Berlev was Gasolin's drummer, known for his solid and dynamic drumming." }
+            new BandMember { Name = "Kim Larsen", Role = "Vocals, Guitar", Biography = "Kim Larsen was the lead singer and guitarist of Gasolin, known for his poetic lyrics and charismatic stage presence.",ImagePath = "/Images/Kim.jpg "},
+            new BandMember { Name = "Franz Beckerlee", Role = "Guitar", Biography = "Franz Beckerlee was the band's lead guitarist, famous for his raw and energetic playing style.",ImagePath = "/Images/franz.jpg " },
+            new BandMember { Name = "Wili Jønsson", Role = "Bass", Biography = "Wili Jønsson played bass and contributed to the band's unique sound with his melodic style.",ImagePath = "/Images/wili.jpg " },
+            new BandMember { Name = "Søren Berlev", Role = "Drums", Biography = "Søren Berlev was Gasolin's drummer, known for his solid and dynamic drumming.",ImagePath = "/Images/søren.jpg " }
         };
     }
 }
@@ -22,4 +22,5 @@ public class BandMember
     public string Name { get; set; }
     public string Role { get; set; }
     public string Biography { get; set; }
+    public string ImagePath { get; set; }
 }
