0a921e1c18d1060903e75b076f35910b333b3c77
Andershgras <andershgras@gmail.com>
2025-03-19 10:05:32 +0100

    Memorabilia

---
+++ GasolinWebsite/Pages/Memorabilia.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GasolinWebsite.Pages
+{
+    public class MemorabiliaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
